e9b5564f092c3fc0e0460bef87b7f46aca7e0069
Liliia Baieva <555blv@gmail.com>
2023-09-04 22:59:15 +0300

    Add DataProvider class to use *.csv files

--- src/main/java/com/bookshare/fw/DataProviders.java
+++ src/main/java/com/bookshare/fw/DataProviders.java
@@ -1,2 +1,58 @@
-package com.bookshare.fw;public class DataProvider {
+package com.bookshare.fw;
+
+import com.bookshare.model.User;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+  @DataProvider
+  public Iterator<Object[]> addUserFromCsvFile() throws IOException{
+    List<Object[]> list = new ArrayList<>();
+
+    BufferedReader reader = new BufferedReader(new FileReader(new File(
+        "src/test/resources/user.csv" //TODO
+    )));
+
+    String line = reader.readLine();
+
+    while(line != null){
+      String[] split = line.split(",");
+      list.add(new Object[]{new User()
+                                .setEmail(split[0])
+                                .setPassword(split[1])
+                                .setConfirmPassword(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  public Iterator<Object[]> addUserFromCsvFileNegative() throws IOException{
+    List<Object[]> list = new ArrayList<>();
+
+    BufferedReader reader = new BufferedReader(new FileReader(new File(
+        "src/test/resources/user_negative.csv" //TODO
+    )));
+
+    String line = reader.readLine();
+
+    while(line != null){
+      String[] split = line.split(",");
+      list.add(new Object[]{new User()
+                                .setEmail(split[0])
+                                .setPassword(split[1])
+                                .setConfirmPassword(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+
+
 }
