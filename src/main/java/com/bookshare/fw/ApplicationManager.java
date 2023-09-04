faf5ca1b91b5fc4ed78858e33fe071160552ce18
Liliia Baieva <555blv@gmail.com>
2023-09-04 22:54:56 +0300

    Add ApplicationManager.java

--- src/main/java/com/bookshare/fw/ApplicationManager.java
+++ src/main/java/com/bookshare/fw/ApplicationManager.java
@@ -1,2 +1,51 @@
-package com.bookshare.fw;public class ApplicationManager {
+package com.bookshare.fw;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ApplicationManager {
+
+  String browser;
+
+  WebDriver driver;
+
+  UserHelper user;
+
+  public void setUser(UserHelper user) {
+    this.user = user;
+  }
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
+  public void init () {
+
+    if(browser.equalsIgnoreCase("chrome")){
+      driver = new ChromeDriver();
+    } else if (browser.equalsIgnoreCase("firefox")) {
+      driver = new FirefoxDriver();
+    } else if (browser.equalsIgnoreCase("edge")) {
+      EdgeOptions options = new EdgeOptions();
+      options.addArguments("remote-allow-origins=*");
+      driver = new EdgeDriver(options);
+    }
+
+    driver.get("http://localhost:3000");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+  }
+
+  public void stop(){
+    driver.quit();
+  }
+
+  public UserHelper getUser() {
+    return user;
+  }
 }
