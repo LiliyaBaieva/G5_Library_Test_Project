89e5bbc53997f7f4b129aa9a7d6232c923f7ad80
Liliia Baieva <555blv@gmail.com>
2023-09-04 23:01:08 +0300

    add User class as model

--- src/main/java/com/bookshare/model/User.java
+++ src/main/java/com/bookshare/model/User.java
@@ -1,2 +1,38 @@
-package com.bookshare.model;public class User {
+package com.bookshare.model;
+
+import java.util.Objects;
+
+public class User {
+
+  public String email;
+  public String password;
+  public String confirmPassword;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public User setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public User setPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public String getConfirmPassword() {
+    return confirmPassword;
+  }
+
+  public User setConfirmPassword(String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+    return this;
+  }
+
 }
