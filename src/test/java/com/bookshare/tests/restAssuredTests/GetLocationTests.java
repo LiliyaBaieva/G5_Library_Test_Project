a5690bd551bb5b7ada364cd6827781978b9b2520
Liliia Baieva <555blv@gmail.com>
2023-09-17 18:41:29 +0300

    add GetLocationTests.java

--- src/test/java/com/bookshare/tests/restAssuredTests/GetLocationTests.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/GetLocationTests.java
@@ -1,2 +1,29 @@
-package com.bookshare.tests.restAssuredTests;public class GetLocationTests {
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+public class GetLocationTests extends TestBaseApi{
+
+  @Test
+  public void getlocationResponse200Test(){
+    given().when().get("/api/location/" + randomPostalCode()).then()
+        .assertThat().statusCode(200);
+  }
+
+  @Test
+  public void getlocationCityTest(){
+    given().when().get("/api/location/13599").then()
+        .assertThat().statusCode(200)
+        .body("city", equalTo("Berlin"));
+  }
+
+  @Test
+  public void getlocationResponse404NegativeTest(){
+    given().when().get("/api/location/18754").then()
+        .assertThat().statusCode(404);
+  }
+
 }
