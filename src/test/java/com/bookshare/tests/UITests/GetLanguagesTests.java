e65edbaaa1d7213942fddd157876ad69a2520c08
Liliia Baieva <555blv@gmail.com>
2023-09-17 19:31:07 +0300

    add GetLanguagesTests.java

--- src/test/java/com/bookshare/tests/UITests/GetLanguagesTests.java
+++ src/test/java/com/bookshare/tests/UITests/GetLanguagesTests.java
@@ -1,2 +1,41 @@
-package com.bookshare.tests.UITests;public class GetLanguagesTests {
+package com.bookshare.tests.UITests;
+
+import static io.restassured.RestAssured.given;
+
+import com.bookshare.tests.restAssuredTests.TestBaseApi;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GetLanguagesTests extends TestBaseApi {
+
+  Cookie sessionCookie;
+
+  @BeforeMethod
+  public void precondition(){
+    sessionCookie = loginWithUser("anna@mail.com", "$Anna.2023$");
+  }
+
+  @Test
+  public void getLanguagesTest(){
+    given()
+        .contentType(ContentType.JSON)
+        .cookie(sessionCookie)
+        .when()
+        .get("/api/languages")
+        .then()
+        .assertThat().statusCode(200);
+  }
+
+  @Test
+  public void getLanguagesWithoutAuthorizationNegativeTest(){
+    given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/api/languages")
+        .then()
+        .assertThat().statusCode(401);
+  }
+
 }
