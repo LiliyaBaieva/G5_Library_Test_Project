5a2c64916ac00828b1ab6ab2f97f886f8f41781e
Liliia Baieva <555blv@gmail.com>
2023-09-17 19:30:36 +0300

    add GetCategoriesTests.java

--- src/test/java/com/bookshare/tests/restAssuredTests/GetCategoriesTests.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/GetCategoriesTests.java
@@ -1,2 +1,23 @@
-package com.bookshare.tests.restAssuredTests;public class GetCategoriesTests {
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GetCategoriesTests extends TestBaseApi{
+
+  @Test
+  public void getCategoriesTest(){
+    given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/api/categories")
+        .then()
+        .assertThat().statusCode(200);
+  }
+
+
 }
