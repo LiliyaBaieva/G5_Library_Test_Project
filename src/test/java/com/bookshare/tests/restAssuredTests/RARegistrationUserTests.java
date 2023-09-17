936be277a816c765ab1d15f9fbe723674a563b76
Liliia Baieva <555blv@gmail.com>
2023-09-17 18:38:44 +0300

    add RARegistrationUserTests

--- src/test/java/com/bookshare/tests/restAssuredTests/RARegistrationUserTests.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/RARegistrationUserTests.java
@@ -1,2 +1,34 @@
-package com.bookshare.tests.restAssuredTests;public class RARegistrationUserTests {
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+
+import com.bookshare.dto.AuthResponseDto;
+import com.bookshare.dto.RegistrationUserDto;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class RARegistrationUserTests extends TestBaseApi{
+
+  @Test
+  public void registerUserPositiveTest(){
+
+    RegistrationUserDto user = RegistrationUserDto.builder()
+        .email("jon" + randomInt() + "@mail.com")
+        .password("Qwerty$333")
+        .confirmPassword("Qwerty$333")
+        .build();
+
+    AuthResponseDto response = given()
+        .contentType(ContentType.JSON)
+        .body(user)
+        .when()
+        .post("/api/registration")
+        .then()
+        .assertThat().statusCode(201)
+        .extract().response().as(AuthResponseDto.class);
+
+    System.out.println(response);
+
+  }
+
 }
--- src/test/java/com/bookshare/tests/restAssuredTests/TestBaseApi.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/TestBaseApi.java
@@ -1,8 +1,15 @@
 package com.bookshare.tests.restAssuredTests;
 
+import static io.restassured.RestAssured.given;
+
+import com.bookshare.dto.UserProfileDto;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.response.Response;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -16,7 +23,7 @@ public class TestBaseApi {
   @BeforeMethod
   public void precondition(Method method, Object[] parametrs){
     RestAssured.baseURI ="http://localhost:8080";
-    RestAssured.basePath = "/v3/api-docs";
+//    RestAssured.basePath = "/v3/api-docs";
     logger.info("Start test " + method.getName() + " with parametrs: " + Arrays.asList(parametrs));
   }
 
@@ -26,5 +33,42 @@ public class TestBaseApi {
     logger.info("========================");
   }
 
+  public static Cookie loginWithUser(String email, String password) {
+    Response loginResponse = given()
+        .contentType(ContentType.fromContentType("application/x-www-form-urlencoded"))
+        .body("username=" + email + "&password=" + password + "")
+        .when()
+        .post("/api/login");
+
+    return loginResponse.getDetailedCookie("JSESSIONID");
+  }
+
+  public UserProfileDto getUserData(Cookie sessionCookie) {
+    return given()
+        .cookie(sessionCookie)
+        .when().get("/api/users/me").then()
+        .extract().response().as(UserProfileDto.class);
+  }
+
+  public static String randomPostalCode() {
+    String postalCode;
+    Random random = new Random();
+    int randomInt = random.nextInt(6);
+    switch (randomInt) {
+      case 1 -> postalCode = "13599";
+      case 2 -> postalCode = "01109";
+      case 3 -> postalCode = "01067";
+      case 4 -> postalCode = "55743";
+      case 5 -> postalCode = "60308";
+      case 6 -> postalCode = "10111";
+      default -> postalCode = "10115";
+    }
+    return postalCode;
+  }
+
+  public Integer randomInt(){
+    Random random = new Random();
+    return random.nextInt(100);
+  }
 
 }
