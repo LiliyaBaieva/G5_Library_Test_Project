84536414f8a88da2e91addbd39e1cd66ee71d66e
Liliia Baieva <555blv@gmail.com>
2023-09-17 18:37:39 +0300

    add logout test

--- src/test/java/com/bookshare/tests/restAssuredTests/RALogOut.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/RALogOut.java
@@ -1,2 +1,28 @@
-package com.bookshare.tests.restAssuredTests;public class RALogOut {
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RALogOut extends TestBaseApi{
+  Cookie sessionCookie;
+
+  @BeforeMethod
+  //login user
+  public void precondition() {
+    sessionCookie = loginWithUser("anna@mail.com", "$Anna.2023$");
+  }
+
+  @Test
+  public void logOutUserTest(){
+    given()
+        .cookie(sessionCookie)
+        .contentType(ContentType.JSON)
+        .when().post("/api/logout").then()
+        .assertThat().statusCode(200);
+  }
+
 }
