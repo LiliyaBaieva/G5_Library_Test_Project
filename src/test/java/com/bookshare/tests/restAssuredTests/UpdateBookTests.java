3b2564f06d695d2efd3e7704f3060d55073613b5
Liliia Baieva <555blv@gmail.com>
2023-09-18 17:39:16 +0300

    add updateBookPositiveTests(), updateBookNotAuthorizedUserNegativeTests(), updateBookNotPermissionUserNegativeTests(), updateNotExistedBookNegativeTests() in UpdateBookTests.java class

--- src/test/java/com/bookshare/tests/restAssuredTests/UpdateBookTests.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/UpdateBookTests.java
@@ -13,10 +13,12 @@ import org.testng.annotations.Test;
 public class UpdateBookTests extends TestBaseApi {
 
   private Cookie cookie;
+  private Cookie cookieNotPermissionUser;
 
   @BeforeMethod
   public void precondition() {
     cookie = loginWithUser("anna@mail.com", "$Anna.2023$");
+    cookieNotPermissionUser = loginWithUser("mark@mail.com", "Qwerty$111");
   }
 
   @Test
@@ -53,5 +55,85 @@ public class UpdateBookTests extends TestBaseApi {
     System.out.println("pages: " + pages);
   }
 
+  @Test
+  public void updateBookNotAuthorizedUserNegativeTests() {
+    int nuOfPages = randomInt() * 10;
+    AddBookDto updatedBook = AddBookDto.builder()
+        .title("Test Book update")
+        .author("Test Author")
+        .description("Some description")
+        .categoryId(1)
+        .languageId(1)
+        .pages(nuOfPages)
+        .publisherDate("2000")
+        .cover(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(updatedBook)
+        .when()
+        .put("/api/books/31")
+        .then()
+        .assertThat().statusCode(401);
+  }
+
+
+  @Test
+  public void updateBookNotPermissionUserNegativeTests() {
+
+    int nuOfPages = randomInt() * 10;
+    AddBookDto updatedBook = AddBookDto.builder()
+        .title("Test Book update")
+        .author("Test Author")
+        .description("Some description")
+        .categoryId(1)
+        .languageId(1)
+        .pages(nuOfPages)
+        .publisherDate("2000")
+        .cover(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+    given()
+        .cookie(cookieNotPermissionUser)
+        .contentType(ContentType.JSON)
+        .body(updatedBook)
+        .when()
+        .put("/api/books/31")
+        .then()
+        .assertThat().statusCode(403);
+  }
+
+
+  @Test
+  public void updateNotExistedBookNegativeTests() {
+
+    int nuOfPages = randomInt() * 10;
+    AddBookDto updatedBook = AddBookDto.builder()
+        .title("Test Book update")
+        .author("Test Author")
+        .description("Some description")
+        .categoryId(1)
+        .languageId(1)
+        .pages(nuOfPages)
+        .publisherDate("2000")
+        .cover(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+    given()
+        .cookie(cookie)
+        .contentType(ContentType.JSON)
+        .body(updatedBook)
+        .when()
+        .put("/api/books/310")
+        .then()
+        .assertThat().statusCode(404);
+  }
 
 }
