b32931e56d5704bc4a33bf5398442f90900dc651
Liliia Baieva <555blv@gmail.com>
2023-09-18 16:22:19 +0300

    add AddBookDto.java class, add addBookPositiveTests(), addBookNotAuthorizedUserNegativeTests(), addBookForUserWithoutDataNegativeTests() in AddBookTests.java class

---
+++ src/main/java/com/bookshare/dto/AddBookDto.java
@@ -0,0 +1,28 @@
+package com.bookshare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class AddBookDto {
+  private String title;
+  private String author;
+  private String description;
+  private Integer categoryId;
+  private Integer languageId;
+  private Integer pages;
+  private String publisherDate;
+  private String cover;
+  private Integer owner;
+
+}
+
---
+++ src/test/java/com/bookshare/tests/restAssuredTests/AddBookTests.java
@@ -0,0 +1,112 @@
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.bookshare.dto.AddBookDto;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddBookTests extends TestBaseApi {
+
+  private Cookie cookieValidUser;
+  private Cookie cookieUserWithoutData;
+  private Cookie cookieUserWithWrongEmail;
+
+  @BeforeMethod
+  public void precondition() {
+     cookieValidUser = loginWithUser("anna@mail.com", "$Anna.2023$");
+     cookieUserWithoutData = loginWithUser("mark@mail.com", "Qwerty$111");
+     cookieUserWithWrongEmail = loginWithUser("anna.mail.com", "$Anna.2023$");
+  }
+
+  @Test
+  public void addBookPositiveTests(){
+    AddBookDto newBook = AddBookDto.builder()
+        .title("Test Book")
+        .author("Test Author")
+        .description("Some description")
+        .categoryId(1)
+        .languageId(1)
+        .pages(200)
+        .publisherDate("2000")
+        .cover("https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+    given()
+        .cookie(cookieValidUser)
+        .contentType(ContentType.JSON)
+        .body(newBook)
+        .when()
+        .post("/api/books")
+        .then()
+        .assertThat().statusCode(201)
+        .assertThat().body("title", equalTo("Test Book"))
+        .assertThat().body("author", equalTo("Test Author"))
+        .assertThat().body("publisherDate", equalTo("2000"));
+  }
+
+ @Test
+  public void addBookNotAuthorizedUserNegativeTests(){
+    AddBookDto newBook = AddBookDto.builder()
+        .title("Test Book negative")
+        .author("Test Author negative")
+        .description("Some description of negative test ")
+        .categoryId(1)
+        .languageId(1)
+        .pages(200)
+        .publisherDate("2000")
+        .cover("https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+given()
+       .contentType(ContentType.JSON)
+       .body(newBook)
+       .when()
+       .post("/api/books")
+       .then()
+       .assertThat().statusCode(401)
+       .assertThat().body("message", containsString("User unauthorized"));
+ }
+
+  @Test
+  public void addBookForUserWithoutDataNegativeTests(){
+    AddBookDto newBook = AddBookDto.builder()
+        .title("Test Book")
+        .author("Test Author")
+        .description("Some description")
+        .categoryId(1)
+        .languageId(1)
+        .pages(200)
+        .publisherDate("2000")
+        .cover("https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png")
+        .owner(10)
+        .build();
+
+    given()
+        .cookie(cookieUserWithoutData)
+        .contentType(ContentType.JSON)
+        .body(newBook)
+        .when()
+        .post("/api/books")
+        .then()
+        .assertThat().statusCode(403);
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
