8fc6647c18537183543d3c2a6758211be1c77ac5
Liliia Baieva <555blv@gmail.com>
2023-09-23 13:56:00 +0300

    added GettingBookTests.java class with gettingBookRegisteredUserPositiveTest(), gettingBookUserAlreadyHaveNegativeTest(), gettingBookNotRegisteredUserNegativeTest(), gettingNotExistBookNegativeTest()

---
+++ src/test/java/com/bookshare/tests/restAssuredTests/GettingBookTests.java
@@ -0,0 +1,74 @@
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GettingBookTests extends TestBaseApi{
+
+  private Cookie cookieAnna;
+  private Cookie cookieN2;
+
+  @BeforeMethod
+  public void precondition() {
+    cookieAnna = loginWithUser("anna@mail.com", "$Anna.2023$");
+    cookieN2 = loginWithUser("n2@gmail.com", "Qwerty007!");
+  }
+
+  @Test
+  public void gettingBookRegisteredUserPositiveTest(){
+
+    given()
+        .cookie(cookieAnna)
+        .contentType(ContentType.JSON)
+        .body("{\"bookId\": 2, \"userId\": 10}")
+        .when()
+        .post("/api/books/getting")
+        .then()
+        .assertThat().statusCode(200);
+  }
+
+  @Test
+  public void gettingBookUserAlreadyHaveNegativeTest(){
+
+    given()
+        .cookie(cookieN2)
+        .contentType(ContentType.JSON)
+        .body("{\"bookId\": 3, \"userId\": 7}")
+        .when()
+        .post("/api/books/getting")
+        .then()
+        .assertThat().statusCode(403);
+  }
+
+  @Test
+  public void gettingBookNotRegisteredUserNegativeTest(){
+
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"bookId\": 2, \"userId\": 10}")
+        .when()
+        .post("/api/books/getting")
+        .then()
+        .assertThat().statusCode(401);
+  }
+
+  @Test
+  public void gettingNotExistBookNegativeTest(){
+
+    given()
+        .cookie(cookieAnna)
+        .contentType(ContentType.JSON)
+        .body("{\"bookId\": 200, \"userId\": 10}")
+        .when()
+        .post("/api/books/getting")
+        .then()
+        .assertThat().statusCode(404);
+  }
+
+//  @AfterMethod
+
+}
