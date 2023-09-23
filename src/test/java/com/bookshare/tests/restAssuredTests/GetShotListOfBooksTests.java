04e7b8d1a75b5e2e3c55d49d34e6c4fb7a9a16f3
Liliia Baieva <555blv@gmail.com>
2023-09-23 22:09:09 +0300

    added GetShotListOfBooksTests.java class with tests  getShotListOfBooksPositiveTest(), getShotListOfBooksWithoutAuthorizationTest()

--- src/test/java/com/bookshare/tests/restAssuredTests/GetShotListOfBooksTests.java
+++ src/test/java/com/bookshare/tests/restAssuredTests/GetShotListOfBooksTests.java
@@ -1,2 +1,55 @@
-package com.bookshare.tests.restAssuredTests;public class GetShotListOfBooksTests {
+package com.bookshare.tests.restAssuredTests;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GetShotListOfBooksTests extends TestBaseApi{
+
+  private Cookie cookieAnna;
+
+  @BeforeMethod
+  public void precondition() {
+    cookieAnna = loginWithUser("anna@mail.com", "$Anna.2023$");
+  }
+
+  @Test
+  public void getShotListOfBooksPositiveTest(){
+    given()
+        .contentType(ContentType.JSON)
+        .cookie(cookieAnna)
+        .when()
+        .get("/api/books")
+        .then()
+        .assertThat().statusCode(200);
+
+  }
+
+    @Test
+  public void getShotListOfBooksWithoutAuthorizationTest(){
+    given()
+        .contentType(ContentType.JSON)
+//        .cookie(cookieAnna)
+        .when()
+        .get("/api/books")
+        .then()
+        .assertThat().statusCode(403);
+
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
